f76cfe336c008a97842e7b86d578b1b7b7dd9fd0
Raya Sodwilai <sodwilai@pdx.edu>
2021-07-20 12:08:43 -0700

    add search function to the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Search from './search';
 
 function App() {
   return (
     <div className="App">
-      Luxury Purse Rental
+      <h1>Luxury Purse Rentals</h1>
+      <Search />
     </div>
   );
 }
